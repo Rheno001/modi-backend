dc3a3cc1a186b2907ea6222e1ffd9b6bfe51aee6
Rheno001 <renfredreuben@gmail.com>
2026-04-30 09:03:57 +0100

    update

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { list, remove } from './users.controller.js';
 import { authMiddleware } from '../../middlewares/auth.middleware.js';
+import { adminMiddleware } from '../../middlewares/role.middleware.js';
 
 const router = Router();
 
 // All user management routes require admin role
-router.use(authMiddleware);
+router.use(authMiddleware, adminMiddleware);
 
 router.get('/', list);
 router.delete('/:id', remove);
