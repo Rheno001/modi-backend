0c680bcaedb6d56e9db83a411d40126db61ddb1b
Rheno001 <renfredreuben@gmail.com>
2026-09-19 17:37:46 +0100

    migration: moved db to neon

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,20 @@
-import 'dotenv/config';
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import "dotenv/config";
+
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    throw new Error("DATABASE_URL is not defined");
+}
 
 const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL!
+    connectionString,
 });
 
-const prisma = new PrismaClient({ adapter }) as PrismaClient;
+const prisma = new PrismaClient({
+    adapter,
+});
 
 export default prisma;
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -124,7 +124,6 @@ export const getMe = async (
         }
 
         return sendSuccess(res, {
-            id: user.id,
             userId: user.id,
             firstName: user.firstName,
             lastName: user.lastName,
